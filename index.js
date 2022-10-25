5bcd8f65bc612add52b6c13c2be00646318ebf16
srbony <srabonycse@gmail.com>
2022-10-25 18:20:58 +0600

    added courses data

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
+const cors = require('cors')
 app.use(cors());
+
+
+const port = process.env.port || 5000;
+const courses = require('./data/Courses.json')
+
+app.get('/', (req, res) => {
+    res.send("courses api running");
+});
+
+app.get('/courses-category', (req, res) => {
+    res.send(courses);
+})
+
 app.listen(port, () => {
     console.log("server is running ", port);
 })
