ae260bb3aaa67e17bc0f65319d8bbe59f0df8bfe
srbony <srabonycse@gmail.com>
2022-10-27 10:26:34 +0600

    create api with id

--- index.js
+++ index.js
@@ -2,18 +2,44 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 app.use(cors());
+const courses = require('./data/Courses.json')
+const allCourses = require('./data/CoursesInfo.json')
 
 
 const port = process.env.port || 5000;
-const courses = require('./data/Courses.json')
+
+app.get('/allCourses', (req, res) => {
+    res.send(allCourses);
+});
 
 app.get('/', (req, res) => {
     res.send("courses api running");
 });
 
+
 app.get('/courses-category', (req, res) => {
     res.send(courses);
-})
+});
+
+
+app.get('/allCourses/:id', (req, res) => {
+    const id = req.params.id;
+    const singleCourse = allCourses.find(course => course.id == id)
+    res.send(singleCourse);
+    if (!singleCourse) {
+        res.send('data not found')
+    }
+    // console.log(req.params);
+});
+
+// app.get('/allCourses/:id', (req, res) => {
+//     console.log(req.params.id);
+//     const singleCourse = allCourses.find(course => course.id == id);
+//     res.send(singleCourse);
+//     if (!singleCourse) {
+//         res.send('data not found')
+//     }
+// });
 
 app.listen(port, () => {
     console.log("server is running ", port);
